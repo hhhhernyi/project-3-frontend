f3089b582c53302dab4d621206da87280f61bd23
unknown <you@example.com>
2025-02-17 15:14:21 +0800

    feat:SignInPromptBox

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-
+import SignInPromptBox from "./components/SignInPromptBox";
 
 const App = () => {
-  return <h1>Hello world!</h1>;
-};
+  return (
+    <>
+    <h1>Hello world!</h1>;
+    <SignInPromptBox />
+    </>
+)};
 
 export default App;
---
+++ src/components/SignInPromptBox.jsx
@@ -0,0 +1,14 @@
+const SignInPromptBox = () => {
+
+    return (
+        <div>
+            <form onSubmit={}>
+            <span>Login:</span>
+            <span>Password:</span>
+            </form>
+        </div>
+    )
+
+}
+
+export default SignInPromptBox;
