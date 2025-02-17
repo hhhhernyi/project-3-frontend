7cccad7108ec4f2419ea2b47895ae9dafd2c9e58
Nicholas Lam <nicholas.lfh91@gmail.com>
2025-02-17 22:48:10 +0800

    feat:prompt box submit form done

--- src/components/SignInPromptBox/SignInPromptBox.jsx
+++ src/components/SignInPromptBox/SignInPromptBox.jsx
@@ -1,6 +1,7 @@
 import Button from "@mui/material/Button";
 import { TextField } from "@mui/material";
 import { useState } from "react";
+import { useNavigate } from "react-router";
 
 const SignInPromptBox = () => {
   const [inputDetails, setInputDetails] = useState({
@@ -14,22 +15,47 @@ const SignInPromptBox = () => {
       username: "",
       password: "",
     });
-};
+  };
 
-const handleInputChange = (e) => {
+  const handleInputChange = (e) => {
     const { name, value } = e.target;
-    setInputDetails({...inputDetails, [name]: value });
-}
-console.log(inputDetails)
-// needs to check for validation, if input details !== db, send error, else navigate()
-return (
+    setInputDetails({ ...inputDetails, [name]: value });
+  };
+  console.log(inputDetails);
+
+  // needs to check for validation, if input details !== db, send error, else navigate()
+
+  const navigate = useNavigate();
+  function navigateToSignUp() {
+    console.log("test");
+    navigate("/sign-up");
+  }
+
+  return (
     <div>
       <form onSubmit={handleSubmit}>
-        <TextField id="usernameLogin" name="username" label="Username:" variant="outlined" value={inputDetails.username} onChange={handleInputChange} />
-        <TextField id="passwordLogin" name="password" label="Password:" variant="outlined" value={inputDetails.password} onChange={handleInputChange} />
+        <TextField
+          id="usernameLogin"
+          name="username"
+          label="Username:"
+          variant="outlined"
+          value={inputDetails.username}
+          onChange={handleInputChange}
+        />
+        <TextField
+          id="passwordLogin"
+          name="password"
+          label="Password:"
+          variant="outlined"
+          value={inputDetails.password}
+          onChange={handleInputChange}
+        />
         <br />
         <Button variant="contained">Submit</Button>
       </form>
+      <Button variant="contained" onClick={navigateToSignUp}>
+        Sign-up
+      </Button>
     </div>
   );
 };
