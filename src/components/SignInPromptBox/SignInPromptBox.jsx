26ad63aabc6354c939781084e026cf1551edaf8e
Nicholas Lam <nicholas.lfh91@gmail.com>
2025-02-17 22:12:13 +0800

    feat:prompt box submit form done

--- src/components/SignInPromptBox/SignInPromptBox.jsx
+++ src/components/SignInPromptBox/SignInPromptBox.jsx
@@ -1,24 +1,37 @@
-import Button from '@mui/material/Button';
-import { TextField } from '@mui/material';
+import Button from "@mui/material/Button";
+import { TextField } from "@mui/material";
+import { useState } from "react";
 
 const SignInPromptBox = () => {
+  const [inputDetails, setInputDetails] = useState({
+    username: "",
+    password: "",
+  });
 
-    const handleSubmit = (e) =>{
-        e.preventDefault();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setInputDetails({
+      username: "",
+      password: "",
+    });
+};
 
-    }
-
-    return (
-        <div>
-            <form onSubmit={handleSubmit} >
-            <TextField id="outlined-basic" label="Username:" variant="outlined" />
-            <TextField id="outlined-basic" label="Password:" variant="outlined" />
-            <br/>
-            <Button variant="contained">Submit</Button>
-            </form>
-        </div>
-    )
-
+const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setInputDetails({...inputDetails, [name]: value });
 }
+console.log(inputDetails)
+
+return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <TextField id="usernameLogin" name="username" label="Username:" variant="outlined" value={inputDetails.username} onChange={handleInputChange} />
+        <TextField id="passwordLogin" name="password" label="Password:" variant="outlined" value={inputDetails.password} onChange={handleInputChange} />
+        <br />
+        <Button variant="contained">Submit</Button>
+      </form>
+    </div>
+  );
+};
 
 export default SignInPromptBox;
