1be7dfe0bcc2138d1196c0ca03d3bac07b5537b5
Hern Yi Lee <hyileenet@gmail.com>
2025-02-18 14:22:10 +0800

    implenmented sign in auth

--- src/components/SignInPromptBox/SignInPromptBox.jsx
+++ src/components/SignInPromptBox/SignInPromptBox.jsx
@@ -1,16 +1,34 @@
 import Button from "@mui/material/Button";
 import { TextField } from "@mui/material";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router";
+import { UserContext } from "../../Contexts/UserContext";
+import * as authService from '../../services/authService'
 
 const SignInPromptBox = () => {
+  const {setUser } = useContext(UserContext)
   const [inputDetails, setInputDetails] = useState({
     username: "",
     password: "",
   });
 
-  const handleSubmit = (e) => {
+  // changed this handle submit to async function
+  // when user submit form, we will run try catch
+  // we will use signIn function from authService to sign in the user
+  // if successful sign in, we will get a token back, saved to a variable signedInUser
+  // setUser is from the userContext, we will use it to set the state of User to signedInUser
+  // navigate to home page
+  // if sign in fail, error message will show (maybe can use prompt)
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    try {
+      console.log('login details: ',inputDetails)
+      const signedInUser = await authService.signIn(inputDetails)
+      setUser(signedInUser)
+      navigate('/home')
+    } catch (err) {
+      console.log(err)
+    }
     setInputDetails({
       username: "",
       password: "",
@@ -21,7 +39,6 @@ const SignInPromptBox = () => {
     const { name, value } = e.target;
     setInputDetails({ ...inputDetails, [name]: value });
   };
-  console.log(inputDetails);
 
   // needs to check for validation, if input details !== db, send error, else navigate()
 
@@ -51,7 +68,7 @@ const SignInPromptBox = () => {
           onChange={handleInputChange}
         />
         <br />
-        <Button variant="contained">Submit</Button>
+        <Button variant="contained" type="submit">Submit</Button>
       </form>
       <Button variant="contained" onClick={navigateToSignUp}>
         Sign-up
--- src/services/clientsService.js
+++ src/services/clientsService.js
@@ -2,7 +2,9 @@ const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/clients`;
 
 async function index() {
     try {
-        const res = await fetch(BASE_URL);
+        const res = await fetch(BASE_URL, {
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+          });
           return  res.json();
     } catch (err) {
         console.log(err);
