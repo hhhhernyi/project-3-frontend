6387736cdd4cb315a245d0080ccf7ec3ec9351f5
Hern Yi Lee <hyileenet@gmail.com>
2025-02-18 22:50:23 +0800

    product services update

---
+++ src/services/productService.js
@@ -0,0 +1,50 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/products`;
+
+
+// view all clients
+async function indexProduct() {
+    try {
+        const res = await fetch(BASE_URL, {
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+          });
+          return  res.json();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// create a new client
+async function createProduct(productFormData) {
+    try {
+        const res = await fetch(BASE_URL, {
+            method: 'POST',
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}`,
+            'Content-Type': 'application/json',
+         },
+         body: JSON.stringify(productFormData),
+          });
+          return  res.json();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+// show a single client
+async function showProduct(productId) {
+    try {
+        const res = await fetch(`${BASE_URL}/${productId}`, {
+            headers: { Authorization: `Bearer ${localStorage.getItem('token')}` },
+          })
+          return res.json()
+
+    } catch (err) {
+        console.log(err)
+    }
+    
+}
+
+export {
+    indexProduct,
+    createProduct,
+    showProduct
+  };
