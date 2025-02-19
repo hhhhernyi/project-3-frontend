fecc6850fdd4896f800ba0aac79556b0734c7acb
Hern Yi Lee <hyileenet@gmail.com>
2025-02-19 15:57:59 +0800

    working on new product form

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,23 +1,44 @@
+import { useState } from "react"
+import Button from '@mui/material/Button';
+
 export default function NewProducts() {
+    const [formData, setFormData] = useState({
+        name:'',
+        company:'',
+        category:'',
+        link:''
+    })
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log(formData)
+    }
+    function handleChange(event) {
+        setFormData({ ...formData, [event.target.name]: event.target.value });
+    }
     return (
         <>
         <h1>create a new product</h1>
-        <form >
-            <label>name: </label>
-            <input></input>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="name">name: </label>
+            <input id='name' onChange={handleChange} name='name'></input>
 
-            <label>company:</label>
-            <input></input>
+            <label htmlFor="company">company:</label>
+            <input id='company' onChange={handleChange} name='company'></input>
             
-            <label>category:</label>
-            <select>
-                <options>Life</options>
-                <options>Hospital</options>
-                <options>Accident</options>
+            <label htmlFor="category">category:</label>
+            <select id='category' onChange={handleChange} name='category'>
+                <option value='' disabled selected>Select Category</option>
+                <option value='Life'>Life</option>
+                <option value='Hospitalization'>Hospitalization</option>
+                <option value='Accident'>Accident</option>
+                <option value='Investment'>Investment</option>
+                <option value='Endowment'>Endowment</option>
             </select>
 
-            <label>link</label>
-            <input></input>
+            <label htmlFor="link">link</label>
+            <input id='link' onChange={handleChange} name='link'></input>
+
+            <Button className='signUpPageButton' variant="outlined" type='submit'>Create Product!</Button>
 
         </form>
 
