7404fa52eec614fc1710e4cf07f9e07e6f2a72b6
Hern Yi Lee <hyileenet@gmail.com>
2025-02-20 15:25:01 +0800

    style homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,39 @@
+import { useContext } from "react";
 import Navbar from "../components/Navbar";
-
+import { UserContext } from "../Contexts/UserContext";
+import { Gauge } from "@mui/x-charts";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
 
 const HomePage = () => {
+  const { user } = useContext(UserContext);
   return (
-    <div>
+    <div className="HomePage">
       <Navbar />
-      <h1>Welcome!</h1>
+      <h1 className="welcomeText">Welcome {user.username}</h1>
+      <p className="atAGlance">At a glance</p>
+      <div className="overview">
+        <div className="gaugeComponent">
+          <p>Your monthly goal:</p>
+          <Gauge
+            width={200}
+            height={100}
+            value={45}
+            valueMin={0}
+            valueMax={60}
+            startAngle={-90}
+            endAngle={90}
+            text={({ value, valueMax }) => `$${value} / $${valueMax}`}
+          />
+        </div>
+        <div className="calendarComponent">
+          <p>Weekly Calendar</p>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateCalendar />
+          </LocalizationProvider>
+        </div>
+      </div>
     </div>
   );
 };
