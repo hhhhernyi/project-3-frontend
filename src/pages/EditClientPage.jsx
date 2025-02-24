009631fe38a893588ade68a13f55af7414b775dc
weideow <owweide@hotmail.com>
2025-02-24 09:58:17 +0800

    edit client

--- src/pages/EditClientPage.jsx
+++ src/pages/EditClientPage.jsx
@@ -9,14 +9,22 @@ import ResponsiveAppBar from "../components/Navbar";
 export default function EditClientPage () {
     const navigate = useNavigate()
     const {clientId} = useParams()
-    //const [client, setClient] = useState({})
-    const [formData, setFormData] = useState({})
+    const [client, setClient] = useState({})
+    const [formData, setFormData] = useState({
+        name: '',
+        handphoneNumber: '',
+        priority: '',
+        comments: '',
+        lastmet: '',
+        nextAppt: ''
+
+    })
 
     async function handleSubmit(event) {
-        event.preventDefault()
-        console.log('updated form data: ',formData)
-        const updatedClient = await clientService.updateClient(clientId, formData)
-        console.log('updated client: ', updatedClient)
+        event.preventDefault();
+        console.log('updated form data: ',formData);
+        const updatedClient = await clientService.updateClient(clientId, formData);
+        console.log('updated client: ', updatedClient);
         
     }
     function handleChange(event){
@@ -30,8 +38,16 @@ export default function EditClientPage () {
             async function fetchClientDetails() {
                 try {
                     const clientData = await clientService.showClient(clientId);
-                    //setClient(clientData);
-                    setFormData(clientData)
+                    setClient(clientData);
+                    setFormData({
+                        name: clientData.name ||'',
+                        handphoneNumber: clientData.handphoneNumber || '',
+                        priority: clientData.priority || '',
+                        comments: clientData.comments || '',
+                        lastmet: clientData.lastmet || '',
+                        nextAppt: clientData.nextAppt || ''
+
+                    })
                     console.log('client Data: ',clientData)
                 } catch (error){
                     console.error("Failed to fetch client details:", error);
@@ -51,34 +67,51 @@ export default function EditClientPage () {
             </div>
 
             <div>
+                
                 <label>Handphone Number</label>
                 <input type="text" name="handphoneNumber" value={formData.handphoneNumber} onChange={handleChange} />
+                
             </div>
 
             <div>
+                
                 <label>Priority:</label>
-                <select name="priority" value={formData.priority} onChange={handleChange}>
+                <select name="priority" value={formData.priority || ''} onChange={handleChange}>
                      <option value="High">High</option>
                     <option value="Medium">Medium</option>
                     <option value="Low">Low</option>
                 </select>
+                
             </div>
 
             <div>
+                
                 <label htmlFor="comments">Comments:</label>
                 <TextField type="text" name='comments' id="comments" variant="outlined" value={formData.comments} onChange={handleChange}/>
+                
             </div>
 
             <div>
+                
                 <label htmlFor="lastmet">Last Met Date:</label>
                 <TextField type="date" name='lastmet' id="lastmet" variant="outlined" value={formData.lastmet} onChange={handleChange}/>
+                
             </div>
 
             <div>
-                <Button variant="outlined" type='submit' onClick={handleClick}>Confirm Changes</Button>
-                <Button variant="outlined" type='submit' onClick={handleClick}>Go back</Button>
-
+                
+                <label htmlFor="nextAppt">Next Appt:</label>
+                <TextField type="date" name='nextAppt' id="nextAppt" variant="outlined" value={formData.nextAppt} onChange={handleChange}/>
+                
             </div>
+
+            <div>
+                <p><Button variant="outlined" type='submit' onClick={handleClick}>Confirm Changes</Button>
+                <Button variant="outlined" type='submit' onClick={handleClick}>Go back</Button>
+                </p>
+            </div>
+
+            
         </form>
     </div>
   )
